09877ed243bab6fb9501f3756726deb4638c9133
Nicholas Branscombe <ncb4996@rit.edu>
2026-01-01 23:05:53 -0500

    Colors match selection

--- price-calculator.js
+++ price-calculator.js
@@ -1,8 +1,35 @@
 const examples = document.querySelectorAll(".example");
+const preview = document.querySelector(".preview");
+const outline = document.querySelector(".outline");
+const images = [
+    "Images/head.png",
+    "Images/partial.png",
+    "Images/full-partial.png",
+    "Images/planti-full.png",
+    "Images/digi-full.png"
+];
+
+const outlines = [
+    "Images/outline.png",
+    "Images/planti-outline.png"
+]
+function switchColor(){
+
+}
 
 examples.forEach(el => {
     el.addEventListener("click", () => {
         examples.forEach(e => e.classList.remove("active"));
         el.classList.add("active");
+        const index = el.dataset.img;
+        preview.src = images[index];
+
+        if(index === "3"){
+            
+            outline.src = outlines[1];
+        }
+        else{
+            outline.src = outlines[0];
+        }
     });
 });
