e87d9dcdd25b3cfdb63fcb5b1ca536335f682142
Nicholas Branscombe <ncb4996@rit.edu>
2026-01-10 17:20:42 -0500

    Changed main font

--- price-calculator.js
+++ price-calculator.js
@@ -1,6 +1,7 @@
 const examples = document.querySelectorAll(".example");
 const preview = document.querySelector(".preview");
 const outline = document.querySelector(".outline");
+const whiteout = document.querySelector(".white-outline");
 const images = [
     "Images/head.png",
     "Images/partial.png",
@@ -11,7 +12,9 @@ const images = [
 
 const outlines = [
     "Images/outline.png",
-    "Images/planti-outline.png"
+    "Images/planti-outline.png",
+    "Images/white-outline.png",
+    "Images/planti-white-outline.png"
 ]
 function switchColor(){
 
@@ -27,9 +30,11 @@ examples.forEach(el => {
         if(index === "3"){
             
             outline.src = outlines[1];
+            whiteout.src = outlines[3];
         }
         else{
             outline.src = outlines[0];
+            whiteout.src = outlines[2];
         }
     });
 });
