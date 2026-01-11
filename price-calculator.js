9927d462fa966ac130ea13b2f7617f9c97c606fb
Nicholas Branscombe <ncb4996@rit.edu>
2026-01-11 18:09:00 -0500

    Started calculator

--- price-calculator.js
+++ price-calculator.js
@@ -1,4 +1,6 @@
 const examples = document.querySelectorAll(".example");
+const tabs = document.querySelectorAll(".tab");
+const scrolls = document.querySelectorAll(".scroll");
 const preview = document.querySelector(".preview");
 const outline = document.querySelector(".outline");
 const whiteout = document.querySelector(".white-outline");
@@ -16,9 +18,6 @@ const outlines = [
     "Images/white-outline.png",
     "Images/planti-white-outline.png"
 ]
-function switchColor(){
-
-}
 
 examples.forEach(el => {
     el.addEventListener("click", () => {
@@ -38,3 +37,37 @@ examples.forEach(el => {
         }
     });
 });
+
+tabs.forEach(el =>{
+    el.addEventListener("click", () =>{
+        const index = el.dataset.page;
+        tabs.forEach(e => e.classList.remove("enabled"));
+        scrolls.forEach(e => e.classList.remove("enabled"));
+        el.classList.add("enabled");
+        scrolls[index].classList.add("enabled");
+    })
+})
+
+function syncRangeAndNumber(rangeName, numberName) {
+    const range = document.querySelector(`input[name="${rangeName}"]`);
+    const number = document.querySelector(`input[name="${numberName}"]`);
+
+    if (!range || !number) return;
+
+    range.addEventListener("input", () => {
+      number.value = range.value;
+    });
+
+    number.addEventListener("input", () => {
+      let value = Number(number.value);
+
+      if (value < range.min) value = range.min;
+      if (value > range.max) value = range.max;
+
+      number.value = value;
+      range.value = value;
+    });
+  }
+  syncRangeAndNumber("colorRange", "colorNumber");
+  syncRangeAndNumber("detailRange", "detailNumber");
+  syncRangeAndNumber("tongueRange", "tongueNumber");
